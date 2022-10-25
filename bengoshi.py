a7d2855755d1b524028d0b58cdd2c31cf94da873
hachi8855 <98527546+hachi8855@users.noreply.github.com>
2022-10-25 12:42:51 +0900

    first commit

---
+++ bengoshi.py
@@ -0,0 +1,155 @@
+import streamlit as st
+
+
+st.header('倉敷市弁護士一覧')
+
+
+expander = st.expander("倉敷駅周辺")
+
+
+adachi_link = '[安達　祐一](https://adachilaw.jp/)'
+expander.markdown(adachi_link, unsafe_allow_html=True)
+expander.caption('倉敷市鶴形1-4-15')
+
+inoue_link = '[井上　雅雄](http://www.okayama-public-lo.jp/?page_id=5908)'
+expander.markdown(inoue_link, unsafe_allow_html=True)
+expander.caption('倉敷市日吉町442-11')
+
+iwai_link = '[岩井　順一郎](https://www.bengo4.com/okayama/a_33202/l_1205284/)'
+expander.markdown(iwai_link, unsafe_allow_html=True)
+expander.caption('倉敷市昭和2-1-22')
+
+okabe_link = '[岡部　宗茂](https://www.kurashiki-bengoshi.com/)'
+expander.markdown(okabe_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知１丁目5-17')
+
+kawai_link = '[川井　進](https://kurashiki-lawoffice.com/)'
+expander.markdown(kawai_link, unsafe_allow_html=True)
+expander.caption('倉敷市昭和2-1-28')
+
+kiyono_link = '[清野　幸代](https://www.pref.okayama.jp/page/282957.html)'
+expander.markdown(kiyono_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知2丁目12-3')
+
+komatubara_link = '[小松原　玲子](https://tahara-lawoffice.com/)'
+expander.markdown(komatubara_link, unsafe_allow_html=True)
+expander.caption('倉敷市川西町10番2号')
+
+shimada_link = '[島田　恭子](https://aobatyuo-law.jp/)'
+expander.markdown(shimada_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知3-3-1')
+
+tahara_link = '[田原　洋介](https://tahara-lawoffice.com/)'
+expander.markdown(tahara_link, unsafe_allow_html=True)
+expander.caption('倉敷市川西町10番2号')
+
+naganuma_link = '[長沼　徹](https://legalus.jp/lawyer-detail/f3d0dd68-606a-4e05-92b6-4bba913129a5)'
+expander.markdown(naganuma_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知3-2-5')
+
+hamada_link = '[濱田　弘](https://aobatyuo-law.jp/)'
+expander.markdown(hamada_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知3-3-1')
+
+hara_link = '[原　幸徳](https://aobatyuo-law.jp/)'
+expander.markdown(hara_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知3-3-1')
+
+hiziya_link = '[土屋　宏](https://legalus.jp/office/72d5e43d-a8dc-4f4d-8876-633a5475132d?tab=feature)'
+expander.markdown(hiziya_link, unsafe_allow_html=True)
+expander.caption('倉敷市幸町15-22')
+
+hodu_link = '[保津　大輔](https://legalus.jp/lawyer-detail/cbd0e6a7-0472-405e-a439-6f66ae01374e)'
+expander.markdown(hodu_link, unsafe_allow_html=True)
+expander.caption('倉敷市浜町1-11-21')
+
+miura_link = '[三浦　巧](https://legalus.jp/office/0ca470b8-0d77-4f9b-aa39-e7d0c9b794fa'
+expander.markdown(miura_link, unsafe_allow_html=True)
+expander.caption('倉敷市阿知1-7-2')
+
+yamane_link = '[山根　務](https://lawoffice-yamane.com/)'
+expander.markdown(yamane_link, unsafe_allow_html=True)
+expander.caption('倉敷市昭和2-4-14')
+
+yamamoto_link = '[山本　陽一](http://yamamoto-bengoshi.com/gaiyou/)'
+expander.markdown(yamamoto_link, unsafe_allow_html=True)
+expander.caption('倉敷市幸町1-40')
+
+watanabe_link = '[渡辺　慧](http://www.okayama-public-lo.jp/?page_id=5908)'
+expander.markdown(watanabe_link, unsafe_allow_html=True)
+expander.caption('倉敷市日吉町442-11')
+
+
+expander = st.expander("倉敷市その他")
+
+ooguma_link = '[大熊　裕司](https://ooguma-kukita-law.com/)'
+expander.markdown(ooguma_link, unsafe_allow_html=True)
+expander.caption('倉敷市平田628番地2')
+
+kukita_link = '[莖田　信之](https://ooguma-kukita-law.com/)'
+expander.markdown(kukita_link, unsafe_allow_html=True)
+expander.caption('倉敷市平田628番地2')
+
+okamura_link = '[岡邑　祐樹](https://okamura-law-office.com/)'
+expander.markdown(okamura_link, unsafe_allow_html=True)
+expander.caption('倉敷市沖165-1')
+
+okamoto_link = '[岡本　健史](http://www.kurashiki-bengoshi.jp/)'
+expander.markdown(okamoto_link, unsafe_allow_html=True)
+expander.caption('倉敷市玉島1534-9')
+
+kirishima_link = '[切島　一成](http://www.kurashiki-bengoshi.jp/)'
+expander.markdown(kirishima_link, unsafe_allow_html=True)
+expander.caption('倉敷市玉島1534-9')
+
+kondo_link = '[近藤　剛](https://legalus.jp/lawyer-detail/c1c752a5-17c2-4526-aca1-fe1536455099)'
+expander.markdown(kondo_link, unsafe_allow_html=True)
+expander.caption('倉敷市松島563番地')
+
+shibutani_link = '[渋谷　康華](http://www.kurashiki-wakaba.jp/)'
+expander.markdown(shibutani_link, unsafe_allow_html=True)
+expander.caption('倉敷市西阿知町107-1')
+
+shimizu_link = '[清水　善朗](https://www.sosapo.org/lp/senda_lawyer/)'
+expander.markdown(shimizu_link, unsafe_allow_html=True)
+expander.caption('倉敷市五日市奥畑780-1')
+
+shinsyo_link = '[新庄　将彦](http://www.kurashiki-wakaba.jp/)'
+expander.markdown(shinsyo_link, unsafe_allow_html=True)
+expander.caption('倉敷市西阿知町107-1')
+
+senda_link = '[千田　卓司](https://www.sosapo.org/lp/senda_lawyer/)'
+expander.markdown(senda_link, unsafe_allow_html=True)
+expander.caption('倉敷市五日市奥畑780-1')
+
+tani_link = '[谷　和子](http://kurashiki-sougou.com/)'
+expander.markdown(tani_link, unsafe_allow_html=True)
+expander.caption('倉敷市東町7-3')
+
+mizuoka_link = '[水岡 章](https://agoora.co.jp/jiko/bengo/okayama/mizuoka-aw.html)'
+expander.markdown(mizuoka_link, unsafe_allow_html=True)
+expander.caption('倉敷市田ノ上新町14-7')
+
+morishita_link = '[森下　裕貴](https://www.morishita-lawfirm.com/)'
+expander.markdown(morishita_link, unsafe_allow_html=True)
+expander.caption('倉敷市新倉敷駅前4丁目52-1')
+
+
+
+
+
+expander = st.expander("児島")
+
+saito_link = '[齋藤　まど香](http://kojimasougouhouritsu.com/)'
+expander.markdown(saito_link, unsafe_allow_html=True)
+expander.caption('倉敷市児島駅前1-46')
+
+nakada_link = '[中田　雅文](http://kojimasougouhouritsu.com/)'
+expander.markdown(nakada_link, unsafe_allow_html=True)
+expander.caption('倉敷市児島駅前1-46')
+
+murayama_link = '[村山　晃康](http://kojimasougouhouritsu.com/)'
+expander.markdown(murayama_link, unsafe_allow_html=True)
+expander.caption('倉敷市児島駅前1-46')
+
+
